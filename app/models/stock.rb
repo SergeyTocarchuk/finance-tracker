dcaa6f88256d62157a9ee713cda73b006f047828
Serhii T <tocarchuk@gmail.com>
2021-07-03 17:32:58 +0300

    Create and display stock objects in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,7 +4,7 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   secret_token: 'Tsk_016e444900dc407a828f2b71bd9c4b4b',
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker_symbol)
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )
   end
 
 end
